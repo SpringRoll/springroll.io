03b9bf833e8c9659a65ce9fa057cbfe3144b03d6
Jeremy McCurdy <jeremy.mccurdy@redspace.com>
2024-08-16 10:46:56 -0300

    Data for the example. Split it into its own file for readability and to allow for reuse between components.

---
+++ src/components/IndexedDB/exampleData.ts
@@ -0,0 +1,117 @@
+interface BlockData {
+    code: string;
+    text: string;
+    value: string;
+}
+
+const createBlockData = (code: string, text: string, value: string): BlockData => ({
+    code,
+    text,
+    value
+});
+
+export const codeBlocks: Record<string, BlockData> = {
+    openDB:
+        createBlockData(
+            `//Full example
+const testApp = new springroll.Application({
+features: {
+userData: true
+}
+});
+
+const openButton = document.querySelector('#open');
+
+const dbName = document.querySelector('#dbName');
+const storeName = document.querySelector('#storeName');
+const version = document.querySelector('#version');
+
+testApp.container.on('connected', async () => {
+openButton.addEventListener('click', async () => {
+
+//Open database call is also used to create new stores if required
+let openResult = await springroll.UserData.IDBOpen(
+    dbName.value,
+    // note that version expects a number/boolean so it should be cast if required(i.e. from an input field)
+    Number.parseInt(version.value),
+    { stores: [{ storeName: storeName.value }] } //additions parameter. Used to create new store
+);
+
+console.log(openResult);
+});
+});`,
+            'Open Database',
+            'openDB'
+        ),
+    deleteStore: createBlockData(
+        `let deleteResult = await springroll.UserData.IDBOpen(
+dbName.value,
+version.value != "" ? version : null,
+null, // additions parameter unused when deleting
+deleteStoreName.value != "" ? { stores: [{ storeName: deleteStoreName.value }] } : null
+);
+console.log(deleteResult);`,
+        'Delete Store',
+        'deleteStore'
+    ),
+    createRecord: createBlockData(
+        `let createResult = await springroll.UserData.IDBAdd(
+storeName.value,
+recordValue.value,
+recordKey.value
+);
+console.log(createResult);`,
+        'Create Record',
+        'createRecord'
+    ),
+    updateRecord: createBlockData(
+        `let updateResult = await springroll.UserData.IDBUpdate(
+storeName.value,
+recordKey.value,
+recordValue.value
+);
+console.log(updateResult);`,
+        'Update Record',
+        'updateRecord'
+    ),
+    deleteRecord: createBlockData(
+        `let deleteResult = await springroll.UserData.IDBRemove(
+storeName.value,
+recordKey.value
+);
+console.log(deleteResult);`,
+        'Delete Record',
+        'deleteRecord'
+    ),
+    getRecord: createBlockData(
+        `let result = await springroll.UserData.IDBRead(
+storeName.value,
+recordKey.value
+);
+console.log(result);`,
+        'Get Record',
+        'getRecord'
+    ),
+    deleteDB: createBlockData(
+        `let deleteDBResult = await springroll.UserData.IDBDeleteDB(dbName.value);
+console.log(deleteDBResult);`,
+        'Delete Database',
+        'deleteDB'
+    ),
+    closeDB: createBlockData(
+        `let closeResult = await springroll.UserData.IDBClose();
+console.log(closeResult)`,
+        'Close Database',
+        'closeDB'
+    )
+};
+
+export const eventMap: Record<string, string> = {
+    IDBOpen: 'openDB',
+    IDBAdd: 'createRecord',
+    IDBRemove: 'deleteRecord',
+    IDBRead: 'getRecord',
+    IDBUpdate: 'updateRecord',
+    IDBClose: 'closeDB',
+    IDBDeleteDB: 'deleteDB',
+};
