ee9bf0c06937b973df8fd0bd76c10c2d329fbb46
Jeremy McCurdy <jeremy.mccurdy@redspace.com>
2024-10-01 14:40:57 -0300

    Updating the homepage PBS KIDS copy with the final approved version

--- src/components/PBSKids/index.tsx
+++ src/components/PBSKids/index.tsx
@@ -12,7 +12,7 @@ export default function PBSKids(): JSX.Element {
                     <div className={clsx(styles.logoContainer)}><PBSKidsLogo className={styles.logo} role="img" /></div>
                     <div className={clsx(styles.description)}>
                         <p>PBS KIDS has been actively updating and improving SpringRoll for over a decade. This ecosystem powers hundreds of educational games available on the <Link to="https://pbskids.org/games">PBS KIDS website</Link> and <Link to="https://pbskids.org/apps/play-pbs-kids-games.html">PBS KIDS Games app</Link>, reaching millions of players. <br /> <br />
-                        PBS KIDS collaborates with children’s media producers, game development studios, and curriculum and accessibility advisors. They have built a framework that is stable, scalable, high-performance, and modern.
+                        PBS KIDS collaborates with children’s media producers, game development studios, and curriculum and accessibility advisors. PBS KIDS has built a framework that is stable, scalable, high-performance, and modern.
                             </p></div>
 
                 </div>
